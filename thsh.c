219a514bb5b67bc8704e640dfbf59de3678bb5d8
Luke DiGiacomo <ledigiacomo@unc.edu>
2016-09-24 17:53:52 -0400

    Implement CD and prompt

--- thsh.c
+++ thsh.c
@@ -12,18 +12,23 @@
 #define MAX_PARAMS 1000
 #define pathGLBL getenv("PATH")
 #define HOME getenv("HOME")
+char* pwd;
+char* lwd;
+
 
 struct stat filestat;
 
 void execute(char* path);
-int checkCmd(char* cmd);
+int checkCmd(char* cmd, char** params);
 
 int main(int argc, char ** argv, char **envp) 
 {
   int finished = 0;
   char *prompt = "thsh> ";
   char input[MAX_INPUT];
-  char* pwd = getenv("PWD");
+  pwd = malloc(MAX_PATH_LEN);
+  lwd = malloc(MAX_PATH_LEN);
+  int i;
 
   while (!finished) 
   {
@@ -31,8 +36,11 @@ int main(int argc, char ** argv, char **envp)
     char last_char;
     int rv;
     int count;
+    strcpy(lwd, pwd);
+    getcwd(pwd, MAX_PATH_LEN);
 
-    // Print the prompt and PWD
+
+    // Print the prompt and pwd
     char* pwdPrompt = malloc((strlen(pwd)+strlen(prompt)+3)*sizeof(char));
     sprintf(pwdPrompt, "[%s] %s", pwd, prompt);
     rv = write(1, pwdPrompt, strlen(pwdPrompt));
@@ -67,26 +75,29 @@ int main(int argc, char ** argv, char **envp)
 
     char** params = malloc(MAX_PARAMS * sizeof(char*));
     char* par = strtok(NULL, " ");
-    int i = 0;
+    i = 0;
     //loop through the passed in parameters and store the in char** params
     while(par != NULL)
     {
-      printf("PARAM: %s\n", par);
-      params[i] = par;
-      par = strtok(NULL, " ");
       if(par != NULL && par[strlen(par)-1] == '\n')
         par[strlen(par)-1]='\0';
+      params[i] = par;
+      par = strtok(NULL, " ");
       i++;
     }
 
-    checkCmd(cmd);
+    for(i = 0; params[i] != NULL; i++)
+    {
+      printf("PARAM: %s\n", params[i]);
+    }
+    checkCmd(cmd, params);
 
   }
 
   return 0;
 }
 
-int checkCmd(char* cmd)
+int checkCmd(char* cmd, char** params)
 {
   if(cmd[strlen(cmd)-1] == '\n')
     cmd[strlen(cmd)-1]='\0';
@@ -99,7 +110,11 @@ int checkCmd(char* cmd)
   //not finished
   else if(strcmp(cmd, "cd") == 0)
   {
-    //chdir(arg[0]);
+    if(strcmp(params[0], "-") == 0)
+      chdir(lwd);
+
+    else
+      chdir(params[0]);
   }
 
 	//not search path
@@ -134,6 +149,10 @@ int checkCmd(char* cmd)
       token = strtok(NULL, ":");
       pathI[0]='\0';
 		}
+
+    sprintf(pathI, "%s/%s", pwd, cmd);
+    if(stat(pathI, &filestat) == 0)
+      execute(pathI);
 	}
 }
 
