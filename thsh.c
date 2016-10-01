cc045133a74523f800ebcb76ca496d3312104726
Luke DiGiacomo <ledigiacomo@unc.edu>
2016-09-30 22:43:22 -0400

    add better comments and styling

--- thsh.c
+++ thsh.c
@@ -1,5 +1,8 @@
 /**
   Names: Luke DiGiacomo, Marcus Wallace
+
+  Honor Pledge:  I certify that no unauthorized assistance has been received or given in the completion of this work
+  Signature: Luke DiGiacomo, Marcus Wallace
 */
 
 #include <stdio.h>
@@ -36,6 +39,7 @@ void printHeel();
 
 int main(int argc, char ** argv, char **envp) 
 {
+  setenv("?", "0", 1);
   int finished = 0;
   char *prompt = "thsh> ";
   char input[MAX_INPUT];
@@ -50,7 +54,9 @@ int main(int argc, char ** argv, char **envp)
       fprintf(stderr,"Debug mode on");
     }
 
-    printf("arg[i]: %s\n", argv[i]);
+    if(debug)
+      fprintf(stderr, "arg[i]: %s\n", argv[i]);
+
     if(stat(argv[i], &filestat) == 0)
     {
       int in = open(argv[i], O_RDONLY);
@@ -59,7 +65,8 @@ int main(int argc, char ** argv, char **envp)
       inter = 0;
     }
   }
-
+  
+  //is finished when CMD-C is read from input
   while (!finished) 
   {
     char *cursor; 
@@ -98,7 +105,7 @@ int main(int argc, char ** argv, char **envp)
       break;
     }
 
-    //make input pretty by putting whitespace betweeen all special characters
+    //make input "pretty" by putting whitespace betweeen all special characters
     char* pretty = malloc(strlen(input)*sizeof(char));
     char* inputPt = malloc(strlen(input)*sizeof(char));
     strcpy(inputPt, input);
@@ -107,9 +114,16 @@ int main(int argc, char ** argv, char **envp)
     {
       if(inputPt[0] == '<' || inputPt[0] == '>' || inputPt[0] == '|' || inputPt[0] == '&' || inputPt[0] == '\n')
       {
-        fflush(stdout);
         char* tempOut = malloc((strlen(pretty)+4)*sizeof(char));
         sprintf(tempOut, "%s %c ", pretty, inputPt[0]);
+        pretty = realloc(tempOut, strlen(tempOut)*sizeof(char));
+      }
+
+      //if a tilde is found replace it with $HOME
+      else if(inputPt[0] == '~')
+      {
+        char* tempOut = malloc((strlen(pretty)+strlen(getenv("HOME")))*sizeof(char));
+        sprintf(tempOut, "%s%s", pretty, getenv("HOME"));
         pretty = realloc(tempOut, strlen(tempOut)*sizeof(char));
       }
 
@@ -123,7 +137,7 @@ int main(int argc, char ** argv, char **envp)
     }
 
     if(debug)
-      printf("Pretty: %s", pretty);
+      fprintf(stderr, "Pretty: %s", pretty);
 
     //skip over lines with # (Comments)
     if(input[0] == '#')
@@ -131,7 +145,7 @@ int main(int argc, char ** argv, char **envp)
 
     char* cmd = strtok(pretty, " ");
     if(debug)
-      printf("RUNNING cmd: %s\n", cmd);
+      fprintf(stderr, "RUNNING cmd: %s\n", cmd);
 
     //loop through the passed in parameters and store them in char** params
     char** params = malloc(MAX_PARAMS * sizeof(char*));
@@ -142,6 +156,7 @@ int main(int argc, char ** argv, char **envp)
       if(par != NULL && par[strlen(par)-1] == '\n')
         par[strlen(par)-1]='\0';
 
+      //if param starts with $ change the param to the value of the variable
       if(par[0] == '$')
       {
         char* parp = malloc(strlen(par)*sizeof(char));
@@ -162,12 +177,16 @@ int main(int argc, char ** argv, char **envp)
       i++;
     }
 
+    //check the command to see if it is built it then run the command with the given parameters
     int check = checkCmd(cmd, params);
+    char* ques = malloc(15);
+    sprintf(ques, "%d", check);
+    setenv("?", ques, 1);
     if(debug)
     {
       for(i = 0; params[i] != NULL; i++)
-        printf("PARAM: %s\n", params[i]);
-      printf("ENDED: %s (ret=%d)\n", cmd, check);
+        fprintf(stderr, "PARAM: %s\n", params[i]);
+      fprintf(stderr, "ENDED: %s (ret=%d)\n", cmd, check);
     }
 
     fflush(stdout);
@@ -176,33 +195,39 @@ int main(int argc, char ** argv, char **envp)
   return 0;
 }
 
+//check the command to see if it is built it then run the command with the given parameters
+//will return the return value of exec or a similar value for built-in commands
 int checkCmd(char* cmd, char** params)
 {
   int err;
   int count;
+  //loop through input and look for a <
   for(count = 0; params[count] != NULL; count++)
   {
-  if(strcmp(params[count], "<") == 0)
+    if(strcmp(params[count], "<") == 0)
     {
-        inputFile = params[count+1];
-      if(params[count+2]==NULL){
-       isdirect =1;
-     
-       params[0] ='\0';
-       params[1] = '\0';
-    }
-    else{
-      int i;
       inputFile = params[count+1];
-            for(i=count; params[i+2] != NULL; i++)
+      
+      if(params[count+2]==NULL)
+      {
+        isdirect =1;
+       
+        params[0] ='\0';
+        params[1] = '\0';
+      }
+  
+      else
+      {
+        int i;
+        inputFile = params[count+1];
+        for(i=count; params[i+2] != NULL; i++)
           strcpy(params[i], params[i+2]);
 
         strcpy(params[i], params[i+1]);
         params[i+1] = '\0';
-        
       }
     }
-}
+  }
 
   for(count = 0; params[count] != NULL; count++)
   {
@@ -223,21 +248,21 @@ int checkCmd(char* cmd, char** params)
   {
     exit(3);
   }
-
+  
+  //if the command is set, set the next param to be the value of the next param
   else if(strcmp(cmd, "set") == 0)
   {
-
     char* var = strtok(params[0], "=");
     char* varSet = strtok(NULL, "=");
     if(debug)
     {
-      printf("VAR: %s\n", var);
-      printf("varSet: %s\n", varSet);
+      fprintf(stderr, "VAR: %s\n", var);
+      fprintf(stderr, "varSet: %s\n", varSet);
     }
     setenv(var, varSet, 1);
   }
 
-  //if command is cd execute chdir()
+  //if command is cd execute chdir() after looking for special cases: '' and '-'
   else if(strcmp(cmd, "cd") == 0)
   {
     if(params[0] == NULL)
@@ -245,16 +270,6 @@ int checkCmd(char* cmd, char** params)
 
     else if(strcmp(params[0], "-") == 0)
       err = chdir(lwd);
-
-    else if(params[0][0] == '~')
-    {
-      char* noTil = malloc(strlen(params[0])*sizeof(char));
-      char* newPath = malloc((strlen(params[0])+strlen(getenv("HOME")))*sizeof(char));
-      params[0]++;
-      memcpy(noTil, (params[0] + sizeof(char)), strlen(params[0]));
-      sprintf(newPath, "%s%s", getenv("HOME"), params[0]);
-      err = chdir(newPath);
-    }
 
     else
       err = chdir(params[0]);
@@ -265,28 +280,33 @@ int checkCmd(char* cmd, char** params)
     return err;
   }
 
+  //if command is goheels call printheel to print our ascii art
   else if(strcmp(cmd, "goheels") == 0)
   {
     printHeel();
     return 0;
   }
 
-  //not search path
+  //if the command begins with a '\' look for the value at the absolute address
+  //if found execute it, otherwise, return -1 
   else if(cmd[0] == '\\')
   {
     if(stat(cmd, &filestat) == 0)
     {
       return execute(cmd, params);
     }
+
+    else 
+      return -1;
   }
 
-  //search PATH
+  //search PATH for the command and execute it
   else 
   {
     char* path = malloc(strlen(pathGLBL)*sizeof(char));
     strcpy(path, pathGLBL);
     if(debug)
-      printf("Path: %s\n", path);
+      fprintf(stderr, "Path: %s\n", path);
 
     char* pathI = malloc(MAX_PATH_LEN * sizeof(char));
 
@@ -298,8 +318,8 @@ int checkCmd(char* cmd, char** params)
       sprintf(pathI, "%s/%s", token, cmd);
       if(debug)
       {
-        printf("Pathi: %s\n", pathI);
-        printf("Stat: %d\n", stat(pathI, &filestat));
+        fprintf(stderr, "Pathi: %s\n", pathI);
+        fprintf(stderr, "Stat: %d\n", stat(pathI, &filestat));
       }
 
       if(stat(pathI, &filestat) == 0)
@@ -350,11 +370,11 @@ int execute(char* path, char** params)
     {
       toRun[i+1]=params[i];
       if(debug)
-        printf("arg %d: %s\n", i+1, params[i]);
+        fprintf(stderr, "arg %d: %s\n", i+1, params[i]);
     }
     //printf("cmd is %s", path);
       //   if(isredirect) close(out);
-    status = execv(path, toRun);
+    status = execvp(path, toRun);
     
   }
 
@@ -370,6 +390,8 @@ int execute(char* path, char** params)
   }
 }
 
+//print the ascii art located at the specified location
+//in this case the location is heel.txt in $HOME
 void printHeel()
 {
   char* toHeel = malloc(MAX_PATH_LEN*sizeof(char));
